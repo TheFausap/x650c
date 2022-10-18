2019ca21c526ed8e7d0c8815d86e449230429bac
TheFausap <fausto.saporito@gmail.com>
2022-10-19 00:44:18 +0200

    added microcode sequence, extractions functions

--- source/app.d
+++ source/app.d
@@ -1,5 +1,101 @@
 import std.stdio;
 
+int[] distributor;
+int[] lower_accumulator;
+int[] upper_accumulator;
+int[] program_register;
+int[] op_register;
+int[] address_register;
+int[] control_console_sw;
+
+int[][2000] drum;
+
+void getmem()
+{
+	int ad = 0;
+
+	for(long i=address_register.length-2,j=0;i>=0;i--,j++) {
+		ad += address_register[i] * 10^^j;
+	}
+	switch(ad) {
+	case 8000:
+		control_console_sw[] = program_register[1..11];
+		break;
+	case 8001:
+		program_register[] = distributor[];
+		break;
+	case 8002:
+		program_register[] = lower_accumulator[];
+		break;
+	case 8003:
+		program_register[1..11] = upper_accumulator[];
+		break;
+	default:
+		program_register = drum[ad];
+	}
+}
+
+int[] readmem()
+{
+	int ad = 0;
+
+	for(long i=address_register.length-2,j=0;i>=0;i--,j++) {
+		ad += address_register[i] * 10^^j;
+	}
+	switch(ad) {
+	case 8000:
+		return control_console_sw[];
+	case 8001:
+		return distributor[];
+	case 8002:
+		return lower_accumulator[];
+	case 8003:
+		return upper_accumulator[];
+	default:
+		return drum[ad];
+	}
+}
+
+void writemem(int[] v)
+{
+	int ad = 0;
+
+	for(long i=address_register.length-2,j=0;i>=0;i--,j++) {
+		ad += address_register[i] * 10^^j;
+	}
+	switch(ad) {
+	case 8000:
+		control_console_sw[] = v[1..11];
+		break;
+	case 8001:
+		distributor[] = v[];
+		break;
+	case 8002:
+		lower_accumulator[] = v[];
+		break;
+	case 8003:
+		upper_accumulator[] = v[];
+		break;
+	default:
+		drum[ad] = v[];
+	}
+}
+
+void getop()
+{
+	op_register = program_register[9..11];
+}
+
+void getdaddr()
+{
+	address_register = program_register[5..9];
+}
+
+void getiaddr()
+{
+	address_register = program_register[1..5];
+}
+
 void oned_add(ref int a, ref int b, ref int d, ref int c, int s_a, int s_b)
 {
 	if (s_a == 45) {
@@ -30,6 +126,19 @@ void add1(int[] a)
 	}
 }
 
+void sub1(int[] a)
+{
+	int[] b;
+	b.length = a.length;
+	b[$-1] = 45;
+	b[$-2] = 1;
+	int cc = 0;
+	comp(b);
+	for (long i=a.length-2;i>=0;i--) {
+		oned_add(a[i],b[i],a[i],cc,a[$-1],b[$-1]);
+	}
+}
+
 
 void comp(int[] a)
 {
@@ -39,34 +148,91 @@ void comp(int[] a)
 	a[$-2] = 10-a[$-2];
 }
 
-void add(int[] a, int[] b)
+int add(int[] a, int[] b)
 {
 	int cc = 0;
-	int cf;
 
 	for (long i=a.length-2;i>=0;i--)
 	{
 		oned_add(a[i],b[i],a[i],cc,a[$-1],b[$-1]);
-		cf = cc;
 	}
-	writeln(cc);
-	writeln(cf);
-	if ((a[$-1] == 45) || (b[$-1] == 45)) {
+
+	if ((a[$-1] == 45) ^ (b[$-1] == 45)) {
 		if (cc == 1) {
 			a[$-1] = 43;
 			add1(a);
 			cc = 0;
 		} else {
+			add1(a);
 			comp(a);
 			a[$-1] = 45;
 		}
+	} else if ((a[$-1] == 45) && (b[$-1] == 45)) {
+		comp(a);
+	}
+	return cc;
+}
+
+void opexec()
+{
+	if (op_register == [1,0]) {
+		if (lower_accumulator[$-1] == distributor[$-1]) {
+			add(upper_accumulator,distributor);
+		} else {
+			add(upper_accumulator,distributor);
+			comp(lower_accumulator);
+			sub1(upper_accumulator);
+		}
+	} else if (op_register == [1,5]) {
+		int cry = add(lower_accumulator,distributor);
+		if (cry == 1) {
+			add1(upper_accumulator);
+		}
+	}
+}
+
+void microcode()
+{
+	// phase D
+	getmem();
+	getop();
+	getdaddr();
+
+	// op analysis
+	if (op_register == [1,0]) {
+		distributor[] = readmem();
+	}
+	opexec();
+
+	// phase I
+	op_register[]=0;
+	address_register[]=0;
+	getiaddr();
+
+	// then go to phase D
+}
+
+void init()
+{
+	distributor.length=11;
+	op_register.length=2;
+	address_register.length=11;
+	lower_accumulator.length=11;
+	upper_accumulator.length=10;
+	program_register.length=11;
+	control_console_sw.length=10;
+
+	for(int i=0;i<drum.length;i++) {
+		drum[i].length=11;
 	}
 }
 
 void main()
 {
 	int[] n1 = [0,0,0,3,4,5,'-'];
-	int[] n2 = [0,0,0,7,7,7,'+'];
+	int[] n2 = [0,0,0,7,7,7,'-'];
+
+	init();
 
 	writeln(n1); writeln(n2);
 
